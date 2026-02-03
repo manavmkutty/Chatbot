8d6017ac1e6288d3ace68c1db3c1cf17218ad423
Sreejith Vijayan <sreejithvjn.2005@gmail.com>
2026-02-03 23:12:31 +0530

    assigned tasks

---
+++ chat.py
@@ -0,0 +1,6 @@
+#user query , get -> encode (cheyyan aytt apparth embedding function , import )
+#collection search cheyth similar aytt 5 result edutt vekkanm(variable)
+#aa variable context aytt PROMPT il kodukkenm along with query(user nte)
+
+
+from datastore import embedding_function
